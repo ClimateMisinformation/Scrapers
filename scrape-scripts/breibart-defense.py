5ed99422faa9d5b34e7f8966d4f46ba8df36ee02
Ebru Cucen <ebru.cucen@contino.io>
2020-12-20 14:11:29 +0000

    Revert "#13 fixing location of referenced output file"
    This reverts commit f7e17cd82d1261d12fa40466f72b129f5761e85f.

--- scrape-scripts/breibart-defense.py
+++ scrape-scripts/breibart-defense.py
@@ -20,7 +20,7 @@ from tqdm import tqdm
 
 
 def clean_text(text):
-    return re.sub(' $', '', re.sub('^ ', '', re.sub(' +', ' ', text.replace('\n', ' '))))
+  return re.sub(' $', '', re.sub('^ ', '', re.sub(' +', ' ', text.replace('\n', ' '))))
 
 
 html_to_text_converter = html2text.HTML2Text()
@@ -37,37 +37,42 @@ base_url = 'https://www.breitbart.com'
 urls = []
 
 for link_article_soup in link_list_soup:
-    url = link_article_soup.find('a').attrs['href']
-    urls.append(base_url + url)
+  url = link_article_soup.find('a').attrs['href']
+  urls.append(base_url + url)
 
 articles = {
-    'url': [],
-    'title': [],
-    'author': [],
-    'date': [],
-    'tags': [],
-    'text': [],
+  'url' : [],
+  'title' : [],
+  'author' : [],
+  'date' : [],
+  'tags' : [],
+  'text' : [],
 }
 
 for url in tqdm(urls):
-    response = requests.get(url)
-    page_soup = BeautifulSoup(response.text, 'html.parser')
-    content_soup = page_soup.find('article', 'the-article')
-    header_soup = content_soup.find('header')
-    title = clean_text(header_soup.find('h1').text)
-    author = clean_text(header_soup.find('address').text)
-    date = header_soup.find('time').attrs['datetime'][:10]
-    tags = 'defense'
-    content_soup = content_soup.find('div', 'entry-content')
-    p_list = [html_to_text_converter.handle(
-        str(p_soup)) for p_soup in content_soup.find_all('p')]
-    text = '\n'.join(p_list)
-    articles['url'].append(url)
-    articles['title'].append(title)
-    articles['author'].append(author)
-    articles['date'].append(date)
-    articles['tags'].append(tags)
-    articles['text'].append(text)
+  response = requests.get(url)
+  page_soup = BeautifulSoup(response.text, 'html.parser')
+  content_soup = page_soup.find('article', 'the-article')
+  header_soup = content_soup.find('header')
+  title = clean_text(header_soup.find('h1').text)
+  author = clean_text(header_soup.find('address').text)
+  date = header_soup.find('time').attrs['datetime'][:10]
+  tags = 'defense'
+  content_soup = content_soup.find('div', 'entry-content')
+  p_list = [ html_to_text_converter.handle(str(p_soup)) for p_soup in content_soup.find_all('p') ]
+  text = '\n'.join(p_list)
+  articles['url'].append(url)
+  articles['title'].append(title)
+  articles['author'].append(author)
+  articles['date'].append(date)
+  articles['tags'].append(tags)
+  articles['text'].append(text)
 
 
-pandas.DataFrame(articles).to_csv('../data/breibart-defense.csv', index=False)
+pandas.DataFrame(articles).to_csv('data/breibart-defense.csv', index = False)
+
+
+
+
+
+
